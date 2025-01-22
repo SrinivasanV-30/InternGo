45ef5d3a5c75787b7fd05c2359b76227a3f9f9eb
SrinivasanV-30 <srinivasan@finestcoder.com>
2025-01-22 11:06:20 +0530

    Update index.js

--- internGo-Backend/index.js
+++ internGo-Backend/index.js
@@ -5,7 +5,6 @@ import cors from 'cors';
 
 const app=express();
 const PORT=process.env.PORT || 8000;
-const cors = require('cors');
 
 const corsOptions = {
   origin: 'http://192.168.0.130:5173', 
@@ -13,10 +12,10 @@ const corsOptions = {
   credentials: true, 
 };
 
-app.use(cors(corsOptions));
 
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions));
+
 
 app.use('/api/auth',authRouter);
 app.use('/api/users',userRouter);
