0635c992e1ea67fe0061c2100c9eea3323e4609f
SrinivasanV-30 <srinivasan@finestcoder.com>
2025-02-03 13:50:23 +0530

    msg

--- internGo-Backend/controllers/dailyUpdateController.js
+++ internGo-Backend/controllers/dailyUpdateController.js
@@ -1,4 +1,3 @@
-import { findRoleByName } from "../models/roleModel.js";
 import { findUserForDailyUpdate } from "../models/userModel.js";
 import logger from "../utils/logger.js";
 import sendResponse from "../utils/response.js";
@@ -8,6 +7,12 @@ export const getDailyUpdateByDate=async(req,res)=>{
     try{
         const dailyUpdateData=req.body;
         let whereCondition={};
+        if(dailyUpdateData.name && dailyUpdateData.name.trim!=""){
+            whereCondition.name={
+                contains: dailyUpdateData.name,
+                mode:'insensitive'
+            };
+        }
         if(dailyUpdateData.batch && dailyUpdateData.batch.length>0)
         {
             whereCondition.batch={in:dailyUpdateData.batch}
