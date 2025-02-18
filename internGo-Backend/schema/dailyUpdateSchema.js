bfc468282856ce5d1ece91d3f4749b6ff0fb8ca9
SrinivasanV-30 <srinivasan@finestcoder.com>
2025-02-18 10:36:30 +0530

    dailyUpdate_fix

--- internGo-Backend/schema/dailyUpdateSchema.js
+++ internGo-Backend/schema/dailyUpdateSchema.js
@@ -8,8 +8,8 @@ export const taskSchema = Joi.object({
             taskName: Joi.string().min(3).max(255),
             activitiesPlanned: Joi.string().max(1000),
             activitiesCompleted: Joi.string().allow("").max(1000),
-            estimatedTime: Joi.number().integer().min(1),
-            actualTime: Joi.number().integer().min(0),
+            estimatedTime: Joi.number().min(1),
+            actualTime: Joi.number().min(0),
             taskProgress: Joi.string().valid("PENDING", "COMPLETED")
         }
     })
