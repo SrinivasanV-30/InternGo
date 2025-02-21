2fe273cd1cdd1ee7dd93fe738cc3aa0036fb7460
SrinivasanV-30 <srinivasan@finestcoder.com>
2025-02-21 14:45:25 +0530

    interaction_filter

--- internGo-Backend/controllers/interactionController.js
+++ internGo-Backend/controllers/interactionController.js
@@ -6,129 +6,123 @@ import { sendNotification } from "../services/notificationService.js";
 import { convertTimeStringandDate } from "../helpers/dateTimeHelper.js";
 
 
-export const scheduleInteraction=async(req,res)=>{
-    try{
-        const interactionData=req.body;
+export const scheduleInteraction = async (req, res) => {
+    try {
+        const interactionData = req.body;
         // console.log(interactionData)
         const internNameDetails = await findUserByName(interactionData.assignedIntern);
         const internEmailDetails = await findUserByEmail(interactionData.internEmail);
-        const mentorDetails= await findUserByName(interactionData.assignedMentor);
-        const interviewerDetails= await findUserByName(interactionData.assignedInterviewer);
+        const mentorDetails = await findUserByName(interactionData.assignedMentor);
+        const interviewerDetails = await findUserByName(interactionData.assignedInterviewer);
         // console.log(internNameDetails,internEmailDetails)
-        if(!(internEmailDetails && internNameDetails) )
-        {
+        if (!(internEmailDetails && internNameDetails)) {
             logger.error("Intern not found");
-            return sendResponse(res,404,"Intern not found");
+            return sendResponse(res, 404, "Intern not found");
         }
         // console.log(internEmailDetails,internNameDetails)
-        if(!(JSON.stringify(internEmailDetails)===JSON.stringify(internNameDetails)))
-        {
+        if (!(JSON.stringify(internEmailDetails) === JSON.stringify(internNameDetails))) {
             logger.error("Invalid name and email combination")
-            return sendResponse(res,400,"Invalid name and email combination")
+            return sendResponse(res, 400, "Invalid name and email combination")
         }
-        if(!mentorDetails)
-        {
+        if (!mentorDetails) {
             logger.error("Mentor not found");
-            return sendResponse(res,404,"Mentor not found");
+            return sendResponse(res, 404, "Mentor not found");
         }
-        if(!interviewerDetails)
-        {
+        if (!interviewerDetails) {
             logger.error("Interviewer not found");
-            return sendResponse(res,404,"Interviewer not found");
+            return sendResponse(res, 404, "Interviewer not found");
         }
-        if(!(mentorDetails.role.roleName==='Mentors')){
+        if (!(mentorDetails.role.roleName === 'Mentors')) {
             logger.error("The given mentor name must be of a mentor");
-            return sendResponse(res,403,"The given mentor name must be of a mentor");
+            return sendResponse(res, 403, "The given mentor name must be of a mentor");
         }
-        if(!(interviewerDetails.role.roleName==='Mentors')){
+        if (!(interviewerDetails.role.roleName === 'Mentors')) {
             logger.error("The given interviewer name must be of a mentor");
-            return sendResponse(res,403,"The given interviewer name must be of a mentor");
+            return sendResponse(res, 403, "The given interviewer name must be of a mentor");
         }
-        interactionData.internId=internEmailDetails.id;
-        interactionData.interviewerId=interviewerDetails.id;
-        interactionData.interviewerEmail=interviewerDetails.email;
-        interactionData.date=convertTimeStringandDate(interactionData.date,interactionData.time);
+        interactionData.internId = internEmailDetails.id;
+        interactionData.interviewerId = interviewerDetails.id;
+        interactionData.interviewerEmail = interviewerDetails.email;
+        interactionData.date = convertTimeStringandDate(interactionData.date, interactionData.time);
         console.log(new Date(interactionData.date))
-        const createdInteraction=await createInteractions(interactionData);
+        const createdInteraction = await createInteractions(interactionData);
         logger.info("Scheduled interaction successfully");
-        sendNotification(interactionData.internId,"interaction-scheduled",createdInteraction.id,`Your ${createdInteraction.name} interaction with ${interactionData.assignedInterviewer} is scheduled on ${(new Date(interactionData.date)).toISOString().split("T")[0]} at ${interactionData.time}. Please be prepared.`);
-        sendNotification(interactionData.interviewerId,"interaction-scheduled",createdInteraction.id,`${createdInteraction.name} interaction with ${interactionData.assignedIntern} is scheduled on ${(new Date(interactionData.date)).toISOString().split("T")[0]} at ${interactionData.time}. Please be available.`);
-        sendResponse(res,201,"Scheduled interaction successfully");
+        sendNotification(interactionData.internId, "interaction-scheduled", createdInteraction.id, `Your ${createdInteraction.name} interaction with ${interactionData.assignedInterviewer} is scheduled on ${(new Date(interactionData.date)).toISOString().split("T")[0]} at ${interactionData.time}. Please be prepared.`);
+        sendNotification(interactionData.interviewerId, "interaction-scheduled", createdInteraction.id, `${createdInteraction.name} interaction with ${interactionData.assignedIntern} is scheduled on ${(new Date(interactionData.date)).toISOString().split("T")[0]} at ${interactionData.time}. Please be available.`);
+        sendResponse(res, 201, "Scheduled interaction successfully");
 
     }
-    catch(error){ 
+    catch (error) {
         logger.error(error.message);
     }
 }
 
-export const updateInteraction=async(req,res)=>{
-    try{
-        const id=parseInt(req.params.id);
-        const interactionData=req.body;
+export const updateInteraction = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const interactionData = req.body;
         console.log(req.body)
-        const interactionDetails=await getInteractionById(id);
-        if(!interactionDetails){
+        const interactionDetails = await getInteractionById(id);
+        if (!interactionDetails) {
             logger.error("Interaction not found");
-            return sendResponse(res,404,"Interaction not found")
+            return sendResponse(res, 404, "Interaction not found")
         }
-        if(interactionData.date){
-            if(interactionData.time){
-                interactionData.date=convertTimeStringandDate(interactionData.date,interactionData.time);
+        if (interactionData.date) {
+            if (interactionData.time) {
+                interactionData.date = convertTimeStringandDate(interactionData.date, interactionData.time);
             }
-            else{
-                interactionData.date=convertTimeStringandDate(interactionData.date,interactionDetails.time);
+            else {
+                interactionData.date = convertTimeStringandDate(interactionData.date, interactionDetails.time);
             }
         }
-        if(interactionData.assignedInterviewer){
-            const interviewerDetails= await findUserByName(interactionData.assignedInterviewer);
-            if(!interviewerDetails)
-            {
+        if (interactionData.assignedInterviewer) {
+            const interviewerDetails = await findUserByName(interactionData.assignedInterviewer);
+            if (!interviewerDetails) {
                 logger.error("Interviewer not found");
-                return sendResponse(res,404,"Interviewer not found");
+                return sendResponse(res, 404, "Interviewer not found");
             }
-            if(!(interviewerDetails.role.roleName==='Mentors')){
+            if (!(interviewerDetails.role.roleName === 'Mentors')) {
                 logger.error("The given interviewer name must be of a mentor");
-                return sendResponse(res,403,"The given interviewer name must be of a mentor");
+                return sendResponse(res, 403, "The given interviewer name must be of a mentor");
             }
-            interactionData.interviewerId=interviewerDetails.id;
-            interactionData.interviewerEmail=interviewerDetails.email;
+            interactionData.interviewerId = interviewerDetails.id;
+            interactionData.interviewerEmail = interviewerDetails.email;
 
-            
+
         }
-        if(interactionData.assignedIntern){
-            const interviewerDetails= await findUserByName(interactionData.assignedInterviewer);
-            interactionData.interviewerId=interviewerDetails.id;
-            interactionData.interviewerEmail=interviewerDetails.email;
+        if (interactionData.assignedIntern) {
+            const interviewerDetails = await findUserByName(interactionData.assignedInterviewer);
+            interactionData.interviewerId = interviewerDetails.id;
+            interactionData.interviewerEmail = interviewerDetails.email;
         }
 
-        const updatedInteraction=await updateInteractions(id,interactionData);
-        sendNotification(updatedInteraction.internId,"interaction-updated-schedule",updatedInteraction.id,`Your ${updatedInteraction.name} interaction with ${updatedInteraction.assignedInterviewer} has been rescheduled to ${(new Date(updatedInteraction.date)).toISOString().split("T")[0]} at ${updatedInteraction.time}. Please be prepared.`);
-        sendNotification(updatedInteraction.interviewerId,"interaction-updated-schedule",updatedInteraction.id,`Your ${updatedInteraction.name} interaction with ${updatedInteraction.assignedIntern} has been updated and is now scheduled for ${(new Date(updatedInteraction.date)).toISOString().split("T")[0]} at ${updatedInteraction.time}. Please ensure your availability.`);
+        const updatedInteraction = await updateInteractions(id, interactionData);
+        sendNotification(updatedInteraction.internId, "interaction-updated-schedule", updatedInteraction.id, `Your ${updatedInteraction.name} interaction with ${updatedInteraction.assignedInterviewer} has been rescheduled to ${(new Date(updatedInteraction.date)).toISOString().split("T")[0]} at ${updatedInteraction.time}. Please be prepared.`);
+        sendNotification(updatedInteraction.interviewerId, "interaction-updated-schedule", updatedInteraction.id, `Your ${updatedInteraction.name} interaction with ${updatedInteraction.assignedIntern} has been updated and is now scheduled for ${(new Date(updatedInteraction.date)).toISOString().split("T")[0]} at ${updatedInteraction.time}. Please ensure your availability.`);
         logger.info("Updated interaction details");
-        sendResponse(res,200,"Updated interaction details");
+        sendResponse(res, 200, "Updated interaction details");
 
     }
-    catch(error){
+    catch (error) {
         logger.error(error.message)
     }
 }
 
-export const getInteractionByDateAndFiltering=async(req,res)=>{
-    try{
-        const interactionData=req.body;
-        const limit=parseInt(req.query.limit)||10;
-        const offset=parseInt(req.query.offset)||0;
-    
-        let whereCondition={intern:{},date:{}};
-        if(interactionData.name && interactionData.name.trim!=""){
-            whereCondition.assignedIntern={
+export const getInteractionByDateAndFiltering = async (req, res) => {
+    try {
+        const interactionData = req.body;
+        const limit = parseInt(req.query.limit) || 10;
+        const offset = parseInt(req.query.offset) || 0;
+
+        let whereCondition = { intern: {}, date: {} };
+        if (interactionData.name && interactionData.name.trim != "") {
+            whereCondition.assignedIntern = {
                 contains: interactionData.name,
-                mode:'insensitive'
+                mode: 'insensitive'
             };
         }
-        if(interactionData.batch && interactionData.batch.length>0)
-        {
-            whereCondition.intern.batch={in:interactionData.batch}
+        if (interactionData.batch && interactionData.batch.length > 0) {
+            whereCondition.intern.batch = { in: interactionData.batch }
         }
         if (interactionData.year && interactionData.year.length > 0) {
             whereCondition.intern.year = { in: interactionData.year };
@@ -139,47 +133,101 @@ export const getInteractionByDateAndFiltering=async(req,res)=>{
         if (interactionData.designation && interactionData.designation.length > 0) {
             whereCondition.intern.designation = { in: interactionData.designation };
         }
-        if(interactionData.date)
-        {   
-            whereCondition.date={
+        if (interactionData.date) {
+            whereCondition.date = {
                 gte: new Date(interactionData.date + "T00:00:00.000Z"),
                 lte: new Date(interactionData.date + "T23:59:59.999Z")
             };
-  
+
         }
-        const interactions= await getInteractions(offset,limit,whereCondition);
+        const interactions = await getInteractions(offset, limit, whereCondition);
         const total_items = await interactionCount(whereCondition);
         const total_pages = total_items > 0 ? Math.ceil(total_items / limit) : 0;
         logger.info("Fetched successfully")
-        sendResponse(res,200,"Fetched successfully",{
-            data:interactions,
+        sendResponse(res, 200, "Fetched successfully", {
+            data: interactions,
             total_pages
         });
-        
+
     }
-    catch(error){
+    catch (error) {
         logger.error(error.message);
     }
 }
 
-export const getInteractionByUserId=async(req,res)=>{
-    try{
-        const userId=req.params.id;
-        
-        const userDetails=await findUserByUserId(userId);
-        if(!userDetails){
+export const getInteractionsByUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const interactionData = req.body;
+        const limit = parseInt(req.query.limit) || 10;
+        const offset = parseInt(req.query.offset) || 0;
+        const userDetails = await findUserByUserId(userId);
+
+        if (!userDetails) {
+            logger.error("User not found");
+            return sendResponse(res, 404, "User not found");
+        }
+        let whereCondition = {};
+        if (userDetails.role.roleName === "Mentors") {
+            whereCondition.assignedInterviewer = userDetails.name;
+        }
+
+        if (userDetails.role.roleName === "Interns") {
+            whereCondition.assignedIntern = userDetails.name;
+        }
+
+        if (interactionData.name && !interactionData.name.trim() == "") {
+            whereCondition.assignedIntern = {
+                contains: interactionData.name,
+                mode: 'insensitive'
+            }
+        }
+
+        if (interactionData.interactionStatus && interactionData.interactionStatus.length > 0) {
+            whereCondition.interactionStatus = { in: interactionData.interactionStatus };
+        }
+
+        if (interactionData.date) {
+            whereCondition.date = {
+                gte: new Date(interactionData.date + "T00:00:00.000Z"),
+                lte: new Date(interactionData.date + "T23:59:59.999Z")
+            };
+        }
+
+        const interactions = await getInteractions(offset, limit, whereCondition);
+        const total_items = await interactionCount(whereCondition);
+        const total_pages = total_items > 0 ? Math.ceil(total_items / limit) : 0;
+        logger.info("Fetched successfully")
+        sendResponse(res, 200, "Fetched successfully", {
+            data: interactions,
+            total_pages
+        });
+
+
+    }
+    catch (error) {
+        logger.error(error.message)
+    }
+}
+
+export const getInteractionByUserId = async (req, res) => {
+    try {
+        const userId = req.params.id;
+
+        const userDetails = await findUserByUserId(userId);
+        if (!userDetails) {
             logger.error("User not found")
-            return sendResponse(res,404,"User not found")
+            return sendResponse(res, 404, "User not found")
         }
-        if(userDetails.role.roleName === "Mentors"){
-            return sendResponse(res,200,"Fetched successfully",await getInteractionsTaken(userId));
+        if (userDetails.role.roleName === "Mentors") {
+            return sendResponse(res, 200, "Fetched successfully", await getInteractionsTaken(userId));
         }
-        if(userDetails.role.roleName === "Interns"){
+        if (userDetails.role.roleName === "Interns") {
             logger.info("Fetched successfully");
-            return sendResponse(res,200,"Fetched successfully",await getInteractionsAttended(userId));
+            return sendResponse(res, 200, "Fetched successfully", await getInteractionsAttended(userId));
         }
     }
-    catch(error){
+    catch (error) {
         logger.error(error.message);
     }
 }
@@ -187,9 +235,9 @@ export const getInteractionByUserId=async(req,res)=>{
 export const toggleScheduleStatus = async (req, res) => {
     try {
         const id = parseInt(req.params.id);
-        const isScheduled=req.query.isScheduled==='true'?true:false;
+        const isScheduled = req.query.isScheduled === 'true' ? true : false;
 
-        
+
         const interactionDetails = await getInteractionById(id);
 
         if (!interactionDetails) {
@@ -198,14 +246,14 @@ export const toggleScheduleStatus = async (req, res) => {
         }
 
 
-        await updateInteractions(id, { isScheduled:isScheduled });
-        if(!isScheduled){
-            sendNotification(interactionDetails.internId,"interaction-cancelled",id,`Your interaction with ${interactionDetails.assignedInterviewer} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time} has been cancelled.`);
-            sendNotification(interactionDetails.interviewerId,"interaction-cancelled",id,`Interaction with ${interactionDetails.assignedIntern} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time} has been cancelled.`);
+        await updateInteractions(id, { isScheduled: isScheduled });
+        if (!isScheduled) {
+            sendNotification(interactionDetails.internId, "interaction-cancelled", id, `Your interaction with ${interactionDetails.assignedInterviewer} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time} has been cancelled.`);
+            sendNotification(interactionDetails.interviewerId, "interaction-cancelled", id, `Interaction with ${interactionDetails.assignedIntern} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time} has been cancelled.`);
         }
-        else{
-            sendNotification(interactionDetails.internId,"interaction-scheduled",id,`Your interaction with ${interactionDetails.assignedInterviewer} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time}. Please be prepared.`);
-            sendNotification(interactionDetails.interviewerId,"interaction-scheduled",id,`Interaction with ${interactionDetails.assignedIntern} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time}. Please be available.`);
+        else {
+            sendNotification(interactionDetails.internId, "interaction-scheduled", id, `Your interaction with ${interactionDetails.assignedInterviewer} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time}. Please be prepared.`);
+            sendNotification(interactionDetails.interviewerId, "interaction-scheduled", id, `Interaction with ${interactionDetails.assignedIntern} is scheduled on ${(new Date(interactionDetails.date)).toISOString().split("T")[0]} at ${interactionDetails.time}. Please be available.`);
         }
 
         logger.info("Interaction schedule status updated");
