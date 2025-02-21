cd421c75deea7cf798c40ef5ea332c971da4ebff
SrinivasanV-30 <srinivasan@finestcoder.com>
2025-02-21 15:09:35 +0530

    interaction_fix

--- internGo-Backend/routes/interactionRoutes.js
+++ internGo-Backend/routes/interactionRoutes.js
@@ -7,7 +7,7 @@ const interactionRouter=express.Router();
 
 interactionRouter.post('/schedule',authenticateUser,checkPermission(["interactions.schedule"]),interactionValidation,scheduleInteraction);
 interactionRouter.post('/',authenticateUser,checkPermission(["interactions.schedule"]),getInteractionByDateAndFiltering);
-interactionRouter.get('/:id',authenticateUser,checkPermission(['interactions.view']),checkUser,getInteractionsByUser);
+interactionRouter.post('/:id',authenticateUser,checkPermission(['interactions.view']),checkUser,getInteractionsByUser);
 interactionRouter.patch('/:id/update',authenticateUser,checkPermission(['interactions.schedule']),updateInteraction);
 interactionRouter.get('/:id/toggleSchedule',authenticateUser,checkPermission(['interactions.schedule']),toggleScheduleStatus);
 
