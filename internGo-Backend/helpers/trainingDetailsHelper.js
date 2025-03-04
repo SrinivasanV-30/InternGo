361980da134ea5173f901e487d055f5d27d90eff
SrinivasanV-30 <srinivasan@finestcoder.com>
2025-03-04 18:46:58 +0530

    training_fix

--- internGo-Backend/helpers/trainingDetailsHelper.js
+++ internGo-Backend/helpers/trainingDetailsHelper.js
@@ -4,23 +4,26 @@ import logger from '../utils/logger.js';
 export const trainingDetailsHelper=async(userPlan)=>{
     try{
         let userId=userPlan.id;
-        let currentMilestone;
-        let milestoneCount = 0;
+        let currentMilestone=null;
+       
+        let milestoneDate=new Date(userPlan.plan.startDate);
+        let now=new Date();
+        let userDays=Math.floor((now - userPlan.planStartDate)/(1000*60*60*24));
         for (const milestone of userPlan.plan.milestones) {
             if (!milestone.milestoneDays || isNaN(milestone.milestoneDays)) {
                 logger.error(`Skipping invalid milestone for user ${userId}`);
                 continue;
             }
-
-            if (milestoneCount >= userPlan.daysWorked) {
+            milestoneDate.setDate(milestoneDate.getDate()+milestone.milestoneDays);
+            if (userDays >= Math.floor((milestoneDate-new Date(userPlan.plan.planStartDate))/(1000*60*60*24))) {
                 currentMilestone = milestone;
                 logger.info(`Milestone found for user ${userId}`);
                 
             }
 
-            milestoneCount += milestone.milestoneDays;
+
         }
-        return currentMilestone
+        return currentMilestone;
     }
     catch(error){
         logger.error(error.message)
